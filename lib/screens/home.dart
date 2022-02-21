b6090b11582a09e786c2e2b3293a72542858addb
Alfredo Ucendo <alfredo.ucar@gmail.com>
2022-02-21 23:20:05 +0100

    wrap characters with hero tag for later transition animation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -80,42 +80,45 @@ class Characters extends StatelessWidget {
               onTap: () {
                 print("tap on ${names.elementAt(index)}");
               },
-              child: GridTile(
-                footer: Padding(
-                  padding: const EdgeInsets.only(bottom: 5),
-                  child: Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      Text(
-                        names.elementAt(index),
-                        style: TextStyle(
-                          fontSize: 16,
-                          foreground: Paint()
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth = 3
-                            ..color = Colors.black,
+              child: Hero(
+                tag: names.elementAt(index),
+                child: GridTile(
+                  footer: Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: [
+                        Text(
+                          names.elementAt(index),
+                          style: TextStyle(
+                            fontSize: 16,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 3
+                              ..color = Colors.black,
+                          ),
                         ),
-                      ),
-                      Text(
-                        names.elementAt(index),
-                        style: TextStyle(
-                          fontSize: 16,
-                          foreground: Paint()..color = Colors.white,
+                        Text(
+                          names.elementAt(index),
+                          style: TextStyle(
+                            fontSize: 16,
+                            foreground: Paint()..color = Colors.white,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.only(bottom: 24, top: 5),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2),
-                    borderRadius: BorderRadius.circular(5),
-                    color: Colors.white,
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 24, top: 5),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2),
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                    ),
+                    child: Image(
+                        image: AssetImage(
+                            "lib/assets/characters/${names.elementAt(index)}.png")),
                   ),
-                  child: Image(
-                      image: AssetImage(
-                          "lib/assets/characters/${names.elementAt(index)}.png")),
                 ),
               ),
             );
